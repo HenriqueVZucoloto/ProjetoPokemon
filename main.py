8b19e5b988207d16c3ad13c156c63c4ace469af0
unknown <hzucoloto3@gmail.com>
2021-03-11 15:52:18 -0300

    teste1

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+class Janela:
+    __largura = 0 #largura
+    __altura = 0 #altura
+    __color = (0, 0, 0) #cor
+
+    def __init__(self, largura, altura): #construtor
+        self.__largura = largura
+        self.__altura = altura
+
+    def tamanhoJanela(self): #retorna tamanho da tela
+        return (self.__largura, self.__altura)
+
+    def returnCor(self): #retorna a cor da janela
+        return self.__color
+
+w = Janela(500, 500) #cria objeto janela com dimensões 500x500
+win = pygame.display.set_mode(w.tamanhoJanela()) #inicializa a janela do jogo
