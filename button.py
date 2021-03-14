50ffd8ad83f3e40609dce4152961442f6ef3e7f6
unknown <hzucoloto3@gmail.com>
2021-03-14 19:38:14 -0300

    Segundo commit

---
+++ button.py
@@ -0,0 +1,33 @@
+class Button:
+    '''
+    Propriedades:
+        text,
+        size,
+        position,
+    '''
+    def __init__(self, text, size, position):
+        self.__text = text
+        self.__size = size
+        self.__position = position
+    
+    @property
+    def text(self):
+        return self.__text
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @property
+    def position(self):
+        return self.__position
+
+    def __contains__(self, cursor):
+        x0, y0 = self.position
+        dx, dy = self.size
+        cursorX, cursorY = cursor
+
+        containsX = x0 <= cursorX <= x0 + dx
+        containsY = y0 <= cursorY <= y0 + dy
+
+        return containsX and containsY
--- main.py
+++ main.py
@@ -1,148 +1,139 @@
 import pygame
+from button import Button
+
 pygame.init()
 
-#Cria a janela e muda o nome
-janela = pygame.display.set_mode((800,600)) #inicializa a janela do jogo
+# Cria a janela e muda o nome
+screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption('HOJE TEM GOL DO RIBAMAR')
 
 class Cenas:
-    #Carrega as imagens
+    # Carrega e ajusta as imagens
     def __init__(self):
-        self.fundoBatalha = pygame.image.load('FundoPokemon.png')
-        self.caixaTexto = pygame.image.load('text_bar.png')
-        self.opcoesBatalha = pygame.image.load('fgt_options.png')
-        self.barra1 = pygame.image.load('barra_1.png')
-        self.barra2 = pygame.image.load('barra_2.png')
-        self.caixaGolpes = pygame.image.load('pp_bar.png')
+        self.__background = pygame.image.load('FundoPokemon.png')
+        self.__text_bar = pygame.image.load('text_bar.png')
+        self.__fight_options = pygame.image.load('fgt_options.png')
+        self.__health_bar1 = pygame.image.load('barra_1.png')
+        self.__health_bar2 = pygame.image.load('barra_2.png')
+        self.__pp_bar = pygame.image.load('pp_bar.png')
     
-    #Cria o cenário da batalha
-    def cenarioInicialBatalha(self):
-        #Ajusta o tamanho das imagens
-        self.fundoBatalha = pygame.transform.scale(self.fundoBatalha, (800,440))
-        self.caixaTexto = pygame.transform.scale(self.caixaTexto, (800,160))
-        self.opcoesBatalha = pygame.transform.scale(self.opcoesBatalha, (400, 160))
-        self.barra1 = pygame.transform.scale(self.barra1, (330,100))
-        self.barra2 = pygame.transform.scale(self.barra2, (375,126))
+        self.__background = pygame.transform.scale(self.__background, (800,440))
+        self.__text_bar = pygame.transform.scale(self.__text_bar, (800,160))
+        self.__fight_options = pygame.transform.scale(self.__fight_options, (400, 160))
+        self.__health_bar1 = pygame.transform.scale(self.__health_bar1, (330,100))
+        self.__health_bar2 = pygame.transform.scale(self.__health_bar2, (375,126))
+        self.__pp_bar = pygame.transform.scale(self.__pp_bar, (800,160))
 
-        #Mostra as imagens na tela
-        janela.blit(self.fundoBatalha, (0,0))
-        janela.blit(self.caixaTexto, (0,440))
-        janela.blit(self.opcoesBatalha, (400,440))
-        janela.blit(self.barra1, (30,30))
-        janela.blit(self.barra2, (415,310))
-
-    #Abre a pagina de golpes
-    def paginaGolpes(self):
-        self.caixaGolpes = pygame.transform.scale(self.caixaGolpes, (800,160))
+    # Cria o cenário inicial da batalha
+    def initial(self):
+        screen.blit(self.__background, (0,0))
+        screen.blit(self.__text_bar, (0,440))
+        screen.blit(self.__fight_options, (400,440))
+        screen.blit(self.__health_bar1, (30,30))
+        screen.blit(self.__health_bar2, (415,310))
         
-        cursorX, cursorY = 30, 480
-        golpesAberto = True
-        while golpesAberto:
+        #pygame.mixer.music.play(0)  # Toca a música de batalha
+        
+    # Abre a página de golpes
+    def moves(self):
+        screen.blit(self.__pp_bar, (0,440))
+
+    # Abre a página de batalha
+    def battle(self):
+        screen.blit(self.__text_bar, (0,440))
+        screen.blit(self.__fight_options, (400,440))
+
+#Carrega e printa o cursor        
+cursor = pygame.image.load('cursor.png')
+def blitCursor(cursorX, cursorY):
+    screen.blit(cursor, (cursorX, cursorY))
+
+class Comandos:
+    # Ativa os comandos da pagina de batalha
+    def battle(self):       
+        # Definindo tamanho e texto dos botões
+        button_size = (200, 80)
+        text = ''
+
+        # Definindo os botões
+        fightButton = Button(text, button_size, (400,440))
+        bagButton = Button(text, button_size, (600,440))
+        pokemonButton = Button(text, button_size, (400,520))
+        runButton = Button(text, button_size, (600,520))
+
+        #Posição inicial do cursor
+        cursorX, cursorY = 425, 480
+
+        # Loop da cena de batalha
+        while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
                 
-                #Move o cursor
+                # Movimentos do cursor
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        golpesAberto = False
+                    beep.play()
                     if event.key == pygame.K_DOWN:
-                        janela.blit(self.caixaGolpes, (0,440))
                         cursorY += 53
-                        if cursorY > 550:
-                            cursorY = 533
-                    elif event.key == pygame.K_UP:
-                        janela.blit(self.caixaGolpes, (0,440))
-                        cursorY -= 53
-                        if cursorY < 450:
+                        if cursorY > 533:
                             cursorY = 480
+                    elif event.key == pygame.K_UP:
+                        cursorY -= 53
+                        if cursorY < 480:
+                            cursorY = 533
                     elif event.key == pygame.K_RIGHT:
-                        janela.blit(self.caixaGolpes, (0,440))
-                        cursorX += 240
-                        if cursorX > 270:
-                            cursorX = 270
+                        cursorX += 185
+                        if cursorX > 610:
+                            cursorX = 425
                     elif event.key == pygame.K_LEFT:
-                        janela.blit(self.caixaGolpes, (0,440))
-                        cursorX -= 240
-                        if cursorX < 30:
-                            cursorX = 30
-                    
-                    elif event.key == pygame.K_RETURN:
-                        if cursorX == 30 and cursorY == 480:
-                            print('Golpe 1')
-                            cenas.paginaGolpes()
-                        elif cursorX == 30 and cursorY == 533:
-                            print('Golpe 2')
-                        elif cursorX == 270 and cursorY == 480:
-                            print('Golpe 3')
-                        elif cursorX == 270 and cursorY == 533:
-                            print('Golpe 4')
+                        cursorX -= 185
+                        if cursorX < 425:
+                            cursorX = 610
 
-            janela.blit(self.caixaGolpes, (0,440))
-            posicaoCursor(cursorX, cursorY)
+                    # Ao apertar enter
+                    if event.key == pygame.K_RETURN:
+                        # Ve onde o cursor está e executa
+                        cursor_position = [cursorX, cursorY]
+                        if cursor_position in fightButton:
+                            cenas.moves()
+                            break
+                        elif cursor_position in bagButton:
+                            print('bag')
+                        elif cursor_position in pokemonButton:
+                            print('pokemon')
+                        elif cursor_position in runButton:
+                            print('run') 
+            # Printa a cena e o cursor
+            cenas.battle()
+            blitCursor(cursorX, cursorY)
             pygame.display.update()
+    
 
-    #Abre a pagina da batalha
-    def paginaBatalha(self):
-        janela.blit(self.caixaTexto, (0,440))
-        janela.blit(self.opcoesBatalha, (400,440))
+    #def moves(self):
 
-#Carrega e printa o cursor        
-cursor = pygame.image.load('cursor.png')
-def posicaoCursor(cursorX, cursorY):
-    janela.blit(cursor, (cursorX, cursorY))
 
-#Posição inicial do cursor
-cursorX, cursorY = 425, 480
+            
+
+
+# Carrega os áudios
+pygame.mixer.music.load('batalha.wav')
+pygame.mixer.music.set_volume(0.3)
+beep = pygame.mixer.Sound('beep.wav')
 
 cenas = Cenas()
-cenas.cenarioInicialBatalha()
+comandos = Comandos()
+cenas.initial()
+comandos.battle()
 
-#Loop principal
-jogando = True
-while jogando:
+# Loop principal
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            jogando = False
+            pygame.quit()
+            quit()
 
-        #Ve se alguma tecla foi pressionada
-        if event.type == pygame.KEYDOWN:
-            #Move o cursor
-            if event.key == pygame.K_DOWN:
-                cenas.paginaBatalha()
-                cursorY += 53
-                if cursorY > 533:
-                    cursorY = 533
-            elif event.key == pygame.K_UP:
-                cenas.paginaBatalha()
-                cursorY -= 53
-                if cursorY < 480:
-                    cursorY = 480
-            elif event.key == pygame.K_RIGHT:
-                cenas.paginaBatalha()
-                cursorX += 185
-                if cursorX > 610:
-                    cursorX = 610
-            elif event.key == pygame.K_LEFT:
-                cenas.paginaBatalha()
-                cursorX -= 185
-                if cursorX < 425:
-                    cursorX = 425
-            
-            #Executa o comando selecionado (ENTER)
-            elif event.key == pygame.K_RETURN:
-                if cursorX == 425 and cursorY == 480:
-                    print('Fight')
-                    cenas.paginaGolpes()
-                elif cursorX == 425 and cursorY == 533:
-                    print('Pokémon')
-                elif cursorX == 610 and cursorY == 480:
-                    print('Bag')
-                elif cursorX == 610 and cursorY == 533:
-                    print('Run')
-    
-    cenas.paginaBatalha()
-    posicaoCursor(cursorX, cursorY)
-    
     pygame.display.update()
+
+pygame.quit()
